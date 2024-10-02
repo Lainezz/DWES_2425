88201fdf128189867cc08bc5b2159b2408b9cbb7
Lainezz <diegolinaresmojacar@gmail.com>
2024-10-02 19:15:50 +0200

    codigo 1

---
+++ src/main/java/com/es/tema1/ejerEmail/AppEmail.java
@@ -0,0 +1,9 @@
+package com.es.tema1.ejerEmail;
+
+public class AppEmail {
+
+
+    public static void main(String[] args) {
+        // TODO
+    }
+}
---
+++ src/main/java/com/es/tema1/ejerEmail/model/UserEmail.java
@@ -0,0 +1,28 @@
+package com.es.tema1.ejerEmail.model;
+
+public class UserEmail {
+
+    private String nombre;
+    private String email;
+
+    public UserEmail(String nombre, String email) {
+        this.nombre = nombre;
+        this.email = email;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
