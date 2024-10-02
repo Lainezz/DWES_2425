84cb04202bf13166638b1800ed5118861d58a9a4
Lainezz <diegolinaresmojacar@gmail.com>
2024-10-02 20:02:09 +0200

    adicion de codigo clase

--- src/main/java/com/es/tema1/ejerEmail/controller/UserEmailController.java
+++ src/main/java/com/es/tema1/ejerEmail/controller/UserEmailController.java
@@ -1,5 +1,7 @@
 package com.es.tema1.ejerEmail.controller;
 
+import com.es.tema1.ejerEmail.model.RespuestaHTTP;
+import com.es.tema1.ejerEmail.model.UserEmail;
 import com.es.tema1.ejerEmail.service.UserEmailService;
 
 public class UserEmailController {
@@ -9,6 +11,18 @@ public class UserEmailController {
         this.service = new UserEmailService();
     }
 
+    public RespuestaHTTP getUserEmail(String email) {
 
+        try {
+            UserEmail u = service.getUserEmail(email);
+
+            return u != null ?
+                    new RespuestaHTTP(200, "TODO OK", u) :
+                    new RespuestaHTTP(400, "Bad Request");
+        } catch (Exception e) {
+            return new RespuestaHTTP(500, "Fatal internal Error");
+        }
+
+    }
 
 }
